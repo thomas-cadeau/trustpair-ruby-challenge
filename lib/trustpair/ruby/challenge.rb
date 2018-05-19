2db6ecd48941f16f55317b004d82f4952415f3f0
Thomas Cadeau <thomas@dev-indep.fr>
2018-05-19 18:59:47 +0800

    read the CSV, parse the result set from the API and make a one-call to the API to improve the speed of the result - Need to improve the api call because it's limited actually to 100 results

--- lib/trustpair/ruby/challenge.rb
+++ lib/trustpair/ruby/challenge.rb
@@ -1,11 +1,67 @@
+require 'logger'
+require 'CSV'
 require_relative 'challenge/version'
 require_relative 'opendata/opendata_api'
 
 module Trustpair
   module Ruby
     module Challenge
+      @log = Logger.new(STDOUT)
+
       api = Trustpair::Ruby::Opendata::OpendataApi.new()
-      api.searchBySiret('60203644404227')
+
+      valid_sirets_count = 0
+      companies_before_1950 = 0
+      companies_1950_1975 = 0
+      companies_1976_1995 = 0
+      companies_before_1995_and_2005 = 0
+      companies_after_2005 = 0
+      output = []
+
+      # read CSV and get the companies data from the API
+      content = CSV.read('../../../data/data.csv', {:headers => true})
+      total_input = content['siret'].length
+      companies = api.searchBySirets(content['siret'])
+
+      # loop over the result set of companies found on the open data and parse the results
+      companies.each do |company|
+        record = company['record']['fields']
+        if record
+          output[valid_sirets_count] = {
+              :company_name => record['l1_normalisee'],
+              :siret => record['siret'],
+              :ape => record['apen700'],
+              :legal_nature => record['libnj'],
+              :creation_date => record['dcren'],
+              :address => (record['numvoie'] || '') + ' ' +
+                  (record['typvoie'] || '') + ' ' +
+                  (record['libvoie'] || '') + ' ' +
+                  (record['codpos'] || '') + ' ' +
+                  (record['libcom'] || '')
+          }
+          if record['dcren']
+            creation_date = Date.strptime(record['dcren'], "%Y-%m-%d").year
+            companies_before_1950 += creation_date < 1950 ? 1 : 0
+            companies_1950_1975 += 1950 <= creation_date && creation_date <= 1975 ? 1 : 0
+            companies_1976_1995 += 1976 <= creation_date && creation_date <= 1995 ? 1 : 0
+            companies_before_1995_and_2005 += creation_date < 1995 || creation_date < 2005 ? 1 : 0
+            companies_after_2005 += creation_date > 2005 ? 1 : 0
+          end
+          valid_sirets_count += 1
+        end
+      end
+
+      # TODO: export to a file
+      @log.debug output.to_json
+      # monitoring:
+      @log.info 'Data processing complete'
+      @log.info "* Number of valid SIRETs: [#{valid_sirets_count}]"
+      @log.info "* Number of invalid SIRETs: [#{total_input - valid_sirets_count}]"
+      @log.info "* Number of companies created before 1950: [#{companies_before_1950}]"
+      @log.info "* Number of companies created between 1950 and 1975: [#{companies_1950_1975}]"
+      @log.info "* Number of companies created between 1976 and 1995: [#{companies_1976_1995}]"
+      @log.info "* Number of companies created before 1995 and 2005: [#{companies_before_1995_and_2005}]"
+      @log.info "* Number of companies created after 2005: [#{companies_after_2005}]"
     end
   end
 end
--- lib/trustpair/ruby/opendata/opendata_api.rb
+++ lib/trustpair/ruby/opendata/opendata_api.rb
@@ -31,12 +31,35 @@ module Trustpair
 
           # getting the data
           records = JSON.parse(response.body)['records']
+          record = nil
           if records.empty?
             @log.debug "could not find any record with siret number #{siret}"
           else
             @log.debug "found record #{records[0]['record']} by siret number #{siret}"
-            return records[0]['record']
+            record = records[0]['record']['fields']
           end
+
+          return record
+        end
+
+        def searchBySirets(sirets)
+          sirets_query = ''
+          sirets.each {|siret|
+            sirets_query+=" or (siret%3D#{siret})"
+          }
+          if !sirets_query.empty?
+            sirets_query = sirets_query[4..-1]
+          end
+          # TODO: improve this section to deal with redirects
+          response = Net::HTTP.start(@host, @port, :use_ssl => true) {|http|
+            uri = URI.parse("https://#{@host}:#{@port}#{@main_path}?where=#{sirets_query}&pretty=false&timezone=UTC&rows=100")
+            @log.debug uri
+            http.request(Net::HTTP::Get.new(uri.path + '?' + uri.query))
+          }
+          validate response
+
+          # getting the data
+          JSON.parse(response.body)['records']
         end
 
         private
@@ -45,7 +68,6 @@ module Trustpair
             @log.error("Problem when calling the Opendata API")
             @log.error("Status: #{entity.code}")
             @log.error("Body: #{entity.body}")
-            exit 1
           end
         end
       end
